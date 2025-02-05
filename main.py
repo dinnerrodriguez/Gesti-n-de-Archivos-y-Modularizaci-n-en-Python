b790d9a6ca1ce3f923ad487c7a5797abdfc08138
dinnerrodriguez <dinnerodriguez@gmail.com>
2025-02-04 21:21:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# Proyecto: [Nombre del Proyecto]
+# Proyecto: [Modulación en Python]
 # Estudiante: Prof. Andrés
-# Fecha de Inicio: 2025/02/24
-# Fecha de Entrega: [dd/mm/aaaa]
+# Fecha de Inicio: [04/02/2025]
+# Fecha de Entrega: [04/02/2025]
 # Descripción: Este archivo contiene el punto de entrada principal del proyecto.
 # Recuerda incluir tu nombre completo, la fecha en la que iniciaste el proyecto y la fecha estimada de entrega.
 # Esto ayuda a mantener un registro claro del trabajo realizado.
